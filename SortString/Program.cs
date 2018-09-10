cd6e8033be653f9a636a81424ae8beb0ec68abe6
Myna44 <mynadesign@gmail.com>
2018-09-10 17:08:48 +0200

    String split and join

--- SortString/Program.cs
+++ SortString/Program.cs
@@ -7,9 +7,15 @@ using System.Threading.Tasks;
 namespace SortString
 {
     class Program
-    {
+    {        
+        
         static void Main(string[] args)
         {
+            string w = "Some long string to add to the array";
+            string[] words = w.Split(' ');
+            Array.Sort(words);
+            string s = string.Join(" ", words);
+            Console.WriteLine(s);
         }
     }
 }
